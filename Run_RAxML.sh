5f6bc41cc0cb405a1673a7fedceb8b0ffd901413
Simon Uribe-Convers <uribe-convers@users.noreply.github.com>
2017-08-28 14:03:53 -0400

    Delete Run_RAxML.sh

--- Run_RAxML.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Simple script to call RAxML after pyPHLAWD has finished
-# Simon Uribe-Convers - August 25, 2017 - http://simonuribe.com
-
-ALN=*outaln
-
-raxmlHPC-PTHREADS-AVX2 -T 15 -p 9876 -m GTRCAT -q *_outpart -g *outaln.constraint.tre -s $ALN -n $(echo $ALN)"_450bp.tre"
