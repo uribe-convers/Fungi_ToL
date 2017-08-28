fd9713e36a3271422616fd64fbac9422ee769675
Simon Uribe-Convers <uribe-convers@users.noreply.github.com>
2017-08-28 14:03:24 -0400

    Delete Change_Tip_Names.sh

--- Change_Tip_Names.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Simple script to call the script that renames the tips of the tree 
-# Simon Uribe-Convers - August 25, 2017 - http://simonuribe.com
-
-OUTFILE=RAxML_bestTree*
-
-python /home/brlab/apps/PyPHLAWD/src/change_ncbi_to_name_tre.py *.table $OUTFILE $(echo $OUTFILE)"_Names.tre"
