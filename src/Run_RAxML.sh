f3aa38a19371ba456065c870d0c129a709617950
Simon Uribe-Convers <uribe.convers@gmail.com>
2017-09-29 09:17:34 -0400

    Edited description

--- src/Run_RAxML.sh
+++ src/Run_RAxML.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Simple script to call RAxML after pyPHLAWD has finished
+# It will also remove possible "knuckles" in the constraint tree (requires Phyx)
 # Simon Uribe-Convers - August 25, 2017 - http://simonuribe.com
 
 ALN=*outaln
+knuckles=*outaln.constraint.tre
 
-raxmlHPC-PTHREADS-AVX2 -T 15 -p 9876 -m GTRCAT -q *_outpart -g *outaln.constraint.tre -s $ALN -n $(echo $ALN)"_450bp.tre"
+pxrmk -t $knuckles -o $(echo $knuckles)"_no_knuckles.tre"
+
+
+raxmlHPC-PTHREADS-AVX -T 15 -p 9876 -m GTRCAT -q *_outpart -g *outaln.constraint.tre_no_knuckles.tre -s $ALN -n $(echo $ALN)"_450bp.tre"
